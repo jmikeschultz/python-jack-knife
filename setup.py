9e2503196539facb996f9e51457900ba83215b2a
Mike Schultz <mike.schultz@gmail.com>
2025-07-27 07:25:39 -0400

    tweak

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2024 Mike Schultz
+
 # setup.py
 from setuptools import setup, find_packages
 
--- shell_tests/map_let_reduce.sh
+++ shell_tests/map_let_reduce.sh
@@ -1,2 +1,5 @@
 pjk "{id:1, cars:[{color: 'green', size:1}, {color: 'blue', size:10}]}" [ let:foo:bar reduce:^total+='f.size' over:cars \
     expect:"{id: 1, 'cars': [{color: 'green', size: 1, foo: 'bar'}, {color: 'blue', size: 10, foo: 'bar'}], total: 11}"
+
+pjk '{id:2}' let:foo:'{bar}' -
+									  
